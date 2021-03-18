f76da8fe588df96279045466a589082c4acc2187
ElgunMaqsudzade <elgunvm@code.edu.az>
2021-03-18 20:22:03 +0400

    Deploy for netlify

--- src/pages/messenger.js
+++ src/pages/messenger.js
@@ -1,32 +1,8 @@
 import React from "react";
-import { ChatEngine } from "react-chat-engine";
 
 // Catdirammadiq :((((((((
 export function Messenger() {
-  return (
-    <>
-      <ChatEngine
-        projectID="1f432766-c8fd-4bb1-882b-67b027c11157"
-        userName="Elgun"
-        userSecret="Salam123"
-        height="90vh"
-        renderChatHeader={(chat) => {}}
-        // hideUI={false}
-        // renderChatList={(chatAppState) => {}}
-        // renderNewChatForm={(creds) => {}}
-        // renderChatCard={(chat, index) => {}}
-        // renderChatFeed={(chatAppState) => {}}
-        // renderMessageBubble={(creds, chat, lastMessage, message, nextMessage) => {}}
-        // renderIsTyping={(people) => {}}
-        // renderNewMessageForm={(creds, chatId) => {}}
-        // renderChatSettings={(chatAppState) => {}}
-        // renderChatSettingsTop={(creds, chat) => {}}
-        // renderPeopleSettings={(creds, chat) => {}}
-        // renderPhotosSettings={(creds, chat) => {}}
-        // renderOptionsSettings={(creds, chat) => {}}
-      />
-    </>
-  );
+  return <></>;
 }
 
 export default Messenger;
